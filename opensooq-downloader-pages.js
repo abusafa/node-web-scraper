0a0fc51342c665cb3831f2bd3355c5a354086c54
ihab abusafa <ihab.abusafa@gmail.com>
2017-01-22 14:37:16 +0300

    Update opensooq-downloader-pages.js

--- opensooq-downloader-pages.js
+++ opensooq-downloader-pages.js
@@ -15,13 +15,47 @@ const {
   cat, count=1
 } = argv;
 
-mkdirp.sync(`./public/temp/${cat}`)
+
+let cats = [
+	"عقارات/شقق",
+	"عقارات/فلل-قصور",
+	"عقارات/غرف-للسكن",
+	"عقارات/أبنية-تجارية",
+	"عقارات/عمارات",
+	"عقارات/أراضي-مزارع",
+	"عقارات/شاليهات-مصايف-استراحات",
+	"عقارات/عقارات-اخرى",
+	"سيارات-ومركبات/سيارات-للبيع",
+	"سيارات-ومركبات/سيارات-للإيجار",
+	"سيارات-ومركبات/دراجات-نارية ",
+	"موبايل-تابلت/موبايل",
+	"موبايل-تابلت/تابلت",
+	"موبايل-تابلت/اكسسوارات-موبايل-تابلت",
+	"اجهزة-الكترونيات/لابتوب-كمبيوتر",
+	"اجهزة-الكترونيات/تلفزيون-شاشات",
+	"اجهزة-الكترونيات/كاميرات-تصوير",
+	"اجهزة-الكترونيات/غسالات-ملابس-نشافات",
+	"أثاث-ديكور/أثاث",
+	"أثاث-ديكور/ديكور-المنزل-تأثيث",
+	"أثاث-ديكور/ادوات-المطبخ-ضيافة",
+	"أثاث-ديكور/أدوات-منزلية",
+	"أثاث-ديكور/أثاث-ديكور-اخرى",
+	"لوازم-الأطفال-و-الألعاب/اثاث-وغرف-نوم-اطفال",
+	"لوازم-الأطفال-و-الألعاب/عربات-ومقاعد-أطفال"
+]
+
+
+cats.map((categoty)=> mkdirp.sync(`./public/temp/${categoty}`))
+  
+
 
 MongoClient.connect(url, function(err, db) {
 
   var Posts = db.collection('posts');
 
-  let urls = Posts.find({cat:cat},{limit:count}).toArray(function(err, docs) {
+	
+
+  let urls = Posts.find({},{limit:count}).toArray(function(err, docs) {
     console.log(docs.length);
     docs.map((doc, key)=>{
       console.log(doc);
@@ -35,7 +69,7 @@ MongoClient.connect(url, function(err, db) {
 
 
 queue.process(function (job, done) {
-	let j = `wget -q ${job.data.link} -O - | tr '\n' ' ' | grep -o '<div id="page">.*</div>' > ./public/temp/${cat}/${job.data._id}.html`
+	let j = `wget -q ${job.data.link} -O - | tr '\n' ' ' | grep -o '<div id="page">.*</div>' > ./public/temp/${job.data.cat}/${job.data._id}.html`
   console.log(j);
 	setTimeout(()=>{
 		exec(j, function(code, stdout, stderr) {
